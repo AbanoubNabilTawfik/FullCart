9e6aa02defb41ed2a605a7633e2aa56713bdc194
Abanoub Mansour <amansour@ecapital.com>
2024-01-20 12:57:20 +0200

    add unit of work

---
+++ FullCart/FullCart.Repositories/UOW/UnitOfWork.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace FullCart.Repositories.UOW
+{
+    public class UnitOfWork<T> : IUnitOfWork<T> where T : DbContext
+    {
+        private readonly T _dbContext;
+        public UnitOfWork(T dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public int Save()
+        {
+            var res = _dbContext.SaveChanges();
+            DetachAllEntities();
+            return res;
+        }
+
+        private void DetachAllEntities()
+        {
+            EntityEntry[] entityEntries = _dbContext.ChangeTracker.Entries().ToArray();
+            foreach (EntityEntry entityEntry in entityEntries)
+                entityEntry.State = EntityState.Detached;
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            var res = await _dbContext.SaveChangesAsync();
+            DetachAllEntities();
+            return res;
+        }
+
+        public IDbContextTransaction CurrentTransaction { get; private set; } = null!;
+
+        public IsolationLevel CurrentTransactionIsolationLevel => CurrentTransaction != null ? CurrentTransaction.GetDbTransaction().IsolationLevel : IsolationLevel.Unspecified;
+
+        public IDbContextTransaction BeginTrainsaction(IsolationLevel IsolationLevel = IsolationLevel.Unspecified)
+        {
+            if (IsolationLevel == IsolationLevel.Unspecified)
+                CurrentTransaction = _dbContext.Database.BeginTransaction();
+            else
+                CurrentTransaction = _dbContext.Database.BeginTransaction(IsolationLevel);
+            return CurrentTransaction;
+        }
+        public IExecutionStrategy CreateExecutionStrategy()
+        {
+            return _dbContext.Database.CreateExecutionStrategy();
+        }
+    }
+    class UnitOfWork
+    {
+    }
+}
--- FullCart/FullCart.Services/AutoFacConfiguration.cs
+++ FullCart/FullCart.Services/AutoFacConfiguration.cs
@@ -1,4 +1,6 @@
 ﻿using Autofac;
+using FullCart.Repositories.Generic;
+using FullCart.Repositories.UOW;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +14,12 @@ namespace FullCart.Services
         protected override void Load(ContainerBuilder builder)
         {
             base.Load(builder);
+
+            #region UOW
+            //Register Unit of work service
+            builder.RegisterGeneric(typeof(UnitOfWork<>)).As(typeof(IUnitOfWork<>));
+            #endregion
+
         }
     }
 }
